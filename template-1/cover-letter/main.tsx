92966442e65cc057a1712cd83108a4b15b422aa6
Amirhossein Douzandeh <amirzenoozi72@gmail.com>
2024-11-05 20:59:52 +0100

    :tada: Feat: Add The Main Tex File

---
+++ template-1/cover-letter/main.tsx
@@ -0,0 +1,21 @@
+\documentclass[11pt,onesize,a4paper,titlepage]{main}
+
+
+\def\companyName{Company Name}
+
+\begin{document}
+
+    \name{John Doe}{Software Engineer}
+    \receiver{\companyName}
+
+    \para{Dear \textbf{Hiring Manager},}
+    
+    \para{I am writing to express my interest in the Environmental Analyst position at Green Earth Solutions. With a Bachelor’s degree in Environmental Science from New York University and two years of experience in impact assessment and data analysis at EcoTech Corp, I am excited about the opportunity to contribute to your team.}
+
+    \para{At EcoTech, I managed several sustainability projects, honing my skills in environmental data interpretation and stakeholder communication. I am particularly drawn to Green Earth Solutions’ commitment to innovative environmental strategies, and I am eager to apply my analytical abilities and passion for sustainable development in a forward-thinking organization like yours.}
+
+    \para{Thank you for considering my application. I look forward to the opportunity to discuss how my background aligns with your goals.}
+
+    \bottom{New York, The U.S}{+1 123 450 6789}{joan.doe@email.com}{linkedin-id}
+
+\end{document}
